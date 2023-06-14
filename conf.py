3d56ed7c68d2b57fecc0b1651897004de46c23f1
ciro spataro <cirospat@gmail.com>
2023-06-14 15:02:20 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,9 +14,6 @@ settings_file_name = 'linee-guida-open-data-palermo'
 
 import sys, os
 docs_italia_theme = __import__("docs_italia_theme")
-html_theme = "docs_italia_theme"
-html_theme_path = [docs_italia_theme.get_html_theme_path()]
-
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
@@ -115,7 +112,8 @@ def setup(app):
 
 
 # -- Options for HTML output ----------------------------------------------
-html_theme = 'docs-italia-theme'
+
+html_theme = 'docs_italia_theme'
 
 html_theme_path = [docs_italia_theme.get_html_theme_path()]
 
@@ -133,7 +131,7 @@ html_theme_options = {
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    html_theme = 'docs-italia-theme'
+    html_theme = 'docs_italia_theme'
     #html_theme_path = ["themes", ]
 else:
     # Override default css to get a larger width for ReadTheDoc build
